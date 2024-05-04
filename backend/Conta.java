bc24e030653dcc5cc7554a2ca716768012fb209c
BrendaDantas <brendasod98@gmail.com>
2024-05-03 23:42:46 -0300

    Implementando a classe base - close #1

---
+++ backend/Conta.java
@@ -0,0 +1,37 @@
+package backend;
+
+public class Conta {
+    private String numeroConta;
+    private String nomeTitular;
+    private double saldo;
+
+    public Conta(String numeroConta, String nomeTitular, double saldo) {
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
