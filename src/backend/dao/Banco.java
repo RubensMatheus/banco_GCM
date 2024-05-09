5f332b609d10714e444d22b2f7bb7880bd73a2e2
BrendaDantas <brendasod98@gmail.com>
2024-05-08 23:41:47 -0300

    bugfix 02, Corrigindo bug de transações. Fixes #11.

--- src/backend/dao/Banco.java
+++ src/backend/dao/Banco.java
@@ -31,43 +31,56 @@ public class Banco {
     }
 
     public void creditar(int numero, double valor) {
-        Conta conta = contas.get(numero);
-        if (conta != null) {
-            conta.creditar(valor);
-            System.out.println("Crédito de " + valor + " realizado com sucesso na conta " + numero);
+        if (valor >= 0) {
+            Conta conta = contas.get(numero);
+            if (conta != null) {
+                conta.creditar(valor);
+                System.out.println("Crédito de " + valor + " realizado com sucesso na conta " + numero);
+            } else {
+                System.out.println("Conta não encontrada.");
+            }
         } else {
-            System.out.println("Conta não encontrada.");
+            System.out.println("O valor a ser creditado não pode ser negativo.");
         }
     }
 
     public void debitar(int numero, double valor) {
-        Conta conta = contas.get(numero);
-        if (conta != null) {
-            if (conta.getSaldo() >= valor) {
-                conta.debitar(valor);
-                System.out.println("Débito de " + valor + " realizado com sucesso na conta " + numero);
-            }else {
-                System.out.println("Saldo insuficiente na conta de origem.");
+        if (valor >= 0) {
+            Conta conta = contas.get(numero);
+            if (conta != null) {
+                if (conta.getSaldo() >= valor) {
+                    conta.debitar(valor);
+                    System.out.println("Débito de " + valor + " realizado com sucesso na conta " + numero);
+                }else {
+                    System.out.println("Saldo insuficiente na conta de origem.");
+                }
+            } else {
+                System.out.println("Conta não encontrada.");
             }
         } else {
-            System.out.println("Conta não encontrada.");
+            System.out.println("O valor a ser debitado não pode ser negativo.");
         }
     }
 
     public void transferencia(int origem, int destino, double valor) {
-        Conta contaOrigem = contas.get(origem);
-        Conta contaDestino = contas.get(destino);
+        if (valor >= 0) {
+            Conta contaOrigem = contas.get(origem);
+            Conta contaDestino = contas.get(destino);
 
-        if (contaOrigem != null && contaDestino != null) {
-            if (contaOrigem.getSaldo() >= valor) {
-                contaOrigem.debitar(valor);
-                contaDestino.creditar(valor);
-                System.out.println("Transferência de " + valor + " da conta " + origem + " para a conta " + destino + " realizada com sucesso.");
-            }else {
-                System.out.println("Saldo insuficiente na conta de origem.");
+            if (contaOrigem != null && contaDestino != null) {
+                if (contaOrigem.getSaldo() >= valor) {
+                    contaOrigem.debitar(valor);
+                    contaDestino.creditar(valor);
+                    System.out.println("Transferência de " + valor + " da conta " + origem + " para a conta " + destino + " realizada com sucesso.");
+                }else {
+                    System.out.println("Saldo insuficiente na conta de origem.");
+                }
+            } else {
+                System.out.println("Conta de origem ou destino não encontrada.");
             }
         } else {
-            System.out.println("Conta de origem ou destino não encontrada.");
+            System.out.println("O valor a ser transferido não pode ser negativo.");
+
         }
     }
 }
